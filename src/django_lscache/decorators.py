b9d03889a774066ccdb459586e2ac94278939b6d
Code-Egg <eric780217@gmail.com>
2026-01-13 13:29:14 +0800

    test

--- src/django_lscache/decorators.py
+++ src/django_lscache/decorators.py
@@ -1,20 +1,14 @@
 from functools import wraps
-from django.http import HttpResponse
 
-def lscache(max_age=60):
-    """
-    Decorator for Django views to add LiteSpeed Cache headers.
-
-    Args:
-        max_age (int): Cache duration in seconds.
-    """
+def lscache(max_age=None, cacheability=None, esi=False):
     def decorator(view_func):
         @wraps(view_func)
         def wrapper(request, *args, **kwargs):
-            response = view_func(request, *args, **kwargs)
-            if isinstance(response, HttpResponse):
-                response["X-LiteSpeed-Cache"] = "hit,private"
-                response["Cache-Control"] = f"max-age={max_age}, public"
-            return response
+            request._lscache = {
+                "max_age": max_age,
+                "cacheability": cacheability,
+                "esi": esi
+            }
+            return view_func(request, *args, **kwargs)
         return wrapper
     return decorator
