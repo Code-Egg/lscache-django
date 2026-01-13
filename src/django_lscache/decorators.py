7386c6bc240691ec2d0b299eba3075a218e1bcbd
Code-Egg <eric780217@gmail.com>
2026-01-13 12:45:25 +0800

    test

--- src/django_lscache/decorators.py
+++ src/django_lscache/decorators.py
@@ -1,15 +1,6 @@
-from functools import wraps
+def lscache(view_func):
+    def wrapper(*args, **kwargs):
+        response = view_func(*args, **kwargs)
+        return response
 
-
-def lscache(max_age=60, public=True, esi=False):
-def decorator(view_func):
-@wraps(view_func)
-def _wrapped_view(request, *args, **kwargs):
-parts = [f"max-age={max_age}"]
-parts.append('public' if public else 'private')
-if esi:
-parts.append('esi=on')
-request._lscache_control = ';'.join(parts)
-return view_func(request, *args, **kwargs)
-return _wrapped_view
-return decorator
+    return wrapper
--- src/django_lscache/purging.py
+++ src/django_lscache/purging.py
@@ -1,12 +1,13 @@
 import requests
 
-
 def purge_urls(urls, server_host):
-if not isinstance(urls, (list, tuple)):
-urls = [urls]
-purge_value = ','.join(urls)
-response = requests.request(
-'PURGE', server_host,
-headers={'X-LiteSpeed-Purge': purge_value}
-)
-return response.status_code
+    if not isinstance(urls, (list, tuple)):
+        urls = [urls]
+
+    purge_value = ','.join(urls)
+    response = requests.request(
+        'PURGE',
+        server_host,
+        headers={'X-LiteSpeed-Purge': purge_value}
+    )
+    return response.status_code
