673d2a3f9881f5ef5e053798616394c94b585fa3
Code-Egg <eric780217@gmail.com>
2026-01-13 12:08:23 +0800

    modified:   src/django_lscache/middleware.py

--- src/django_lscache/middleware.py
+++ src/django_lscache/middleware.py
@@ -1,9 +1,21 @@
-from django.utils.deprecation import MiddlewareMixin
+class LSCacheMiddleware:
+    """
+    LiteSpeed Cache middleware for Django
+    """
 
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-class LSCacheMiddleware(MiddlewareMixin):
-def process_response(self, request, response):
-cache_control = getattr(request, '_lscache_control', None)
-if cache_control:
-response['X-LiteSpeed-Cache-Control'] = cache_control
-return response
+    def __call__(self, request):
+        # Before view (optional)
+        response = self.get_response(request)
+        # After view
+        return self.process_response(request, response)
+
+    def process_response(self, request, response):
+        # Example: add LSCache headers
+        from .conf import get_cache_control
+        cache_header = get_cache_control(request)
+        if cache_header:
+            response["X-LiteSpeed-Cache-Control"] = cache_header
+        return response
